f5e6d547c3f775387b6ac94c80e8c5171c33d4d5
Drashti Parikh <parikhdrashti19@gmail.com>
2022-07-22 12:08:43 +0530

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         class="w-full h-full flex flex-col justify-center items-center space-y-10"
       >
         <h1 class="text-6xl font-semibold">Dashboard</h1>
-
+        <!-- ** -->
         <input
           v-model="q"
           label="City"
